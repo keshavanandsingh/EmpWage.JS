0c5b4d6adcb692e1aff25b6d25a9bf375e6f0023
keshavanandsingh <keshavanand22@gmail.com>
2020-11-09 02:23:31 +0530

    abiltiy to calculate wage for full time and part time work

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,9 +1,23 @@
 console.log("Welcome to Employee Wage Computation Program\n"); 
 
-const IS_PRESENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_PRESENT) {
-    console.log("Employee is Present!");
-} else {
-    console.log("Employee is Absent!");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = 4; 
+        break;
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    default:
+        empHrs = 0;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: "+ empWage);
