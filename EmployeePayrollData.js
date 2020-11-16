ab3dd0ac6229b8dbaf5ce60243e124a75e689048
keshavanandsingh <keshavanand22@gmail.com>
2020-11-16 12:19:40 +0530

    added operations to store gender and start date in employee payroll data

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,16 @@ class EmployeePayrollData {
     // properties
     id;
     salary;
+    gender;
+    startDate;
 
     // constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...parameters) {
+        this.id = parameters[0];
+        this.name = parameters[1];
+        this.salary = parameters[2];
+        this.gender = parameters[3];
+        this.startDate = parameters[4];
     }
 
     // getter and setter methods
@@ -20,12 +24,17 @@ class EmployeePayrollData {
 
     // method
     toString() {
-        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
+        const options = { year: "numeric", month: "long", day: "numeric"};
+        const employeeDate = this.startDate === undefined ? "undefined" :
+                                this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary +
+                ", gender=" + this.gender + ", startDate=" + employeeDate;
     }
 }
 
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
-employeePayrollData.name = "Jonathan";
-employeePayrollData.salary = 35000;
+employeePayrollData.name = "Marko";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(2, "Rachel", 40000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
